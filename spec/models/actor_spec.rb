2abaa92663da73f3d024d4e52168caaf1dc209c7
Jack Cullen <jpcullen20@gmail.com>
2020-05-22 10:11:28 -0600

    Add average_age method for actors

--- spec/models/actor_spec.rb
+++ spec/models/actor_spec.rb
@@ -15,5 +15,14 @@ RSpec.describe Actor, type: :model do
       actor2 = movie.actors.create(name: "Middle Age Actor", age: 40)
       expect(Actor.order_by_age).to eq([actor1, actor2, actor3])
     end
+
+    it "average_age" do
+      studio = Studio.create(name: "Funny Studio", location: "Hollywood")
+      movie = studio.movies.create(title: "Dumb n Dumber", year: "1999", genre: "Comedy")
+      actor3 = movie.actors.create(name: "Old Actor", age: 70)
+      actor1 = movie.actors.create(name: "Kid Actor", age: 10)
+      actor2 = movie.actors.create(name: "Middle Age Actor", age: 40)
+      expect(Actor.average_age).to eq(40)
+    end
   end
 end
