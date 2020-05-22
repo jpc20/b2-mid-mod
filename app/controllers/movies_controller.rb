d9ea9b3d8c9b33c49b53111d6fabe46614394aba
Jack Cullen <jpcullen20@gmail.com>
2020-05-22 10:36:21 -0600

    Add actor to movie - finish story 3

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -4,4 +4,10 @@ class MoviesController < ApplicationController
     @movie = Movie.find(params[:id])
   end
 
+  def update
+    movie = Movie.find(params[:id])
+    actor = Actor.find_by(name: params[:name])
+    movie.actors << actor
+    redirect_to "/movies/#{movie.id}"
+  end
 end
