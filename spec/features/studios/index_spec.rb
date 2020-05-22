4728e24e0f22c56d776e668e55502b10ea2d8dce
Jack Cullen <jpcullen20@gmail.com>
2020-05-22 09:31:56 -0600

    Add route to show all studios and their movies - finish story 1

--- spec/features/studios/index_spec.rb
+++ spec/features/studios/index_spec.rb
@@ -2,12 +2,12 @@ require 'rails_helper'
 
 RSpec.describe "Studio Index" do
   it "user can see all studios and the name of their movies" do
-    studio1 = Studio.create(name: "Funny", location: "Hollywood")
-    studio2 = Studio.create(name: "Action", location: "LA")
+    studio1 = Studio.create(name: "Funny Studio", location: "Hollywood")
+    studio2 = Studio.create(name: "Action Studio", location: "LA")
     movie1 = studio1.movies.create(title: "Dumb n Dumber", year: "1999")
     movie2 = studio1.movies.create(title: "Austin Powers", year: "2001")
-    movie3 = studio1.movies.create(title: "Mission Impossible", year: "2004")
-    movie4 = studio1.movies.create(title: "The Bourne Identity", year: "2001")
+    movie3 = studio2.movies.create(title: "Mission Impossible", year: "2004")
+    movie4 = studio2.movies.create(title: "The Bourne Identity", year: "2001")
     visit '/studios'
 
     within ".studio-#{studio1.id}" do
@@ -16,7 +16,7 @@ RSpec.describe "Studio Index" do
       expect(page).to have_content(movie2.title)
     end
 
-    within "studio-#{studio2.id}" do
+    within ".studio-#{studio2.id}" do
       expect(page).to have_content(studio2.name)
       expect(page).to have_content(movie3.title)
       expect(page).to have_content(movie4.title)
